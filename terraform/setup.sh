520702300206b5029b64b58252c4d0a6bc6f32d7
Andrew McIsaac <am43@sanger.ac.uk>
2020-01-23 17:12:08 +0000

    WIP: more user_data config

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+exec >> /var/log/user_data.log 2>&1
+touch /home/ubuntu/test.txt
 
-REPO="git@gitlab.internal.sanger.ac.uk:am43/osdataproc.git"
+REPO="https://gitlab.internal.sanger.ac.uk/am43/osdataproc.git"
 
-ssh-keyscan gitlab.internal.sanger.ac.uk >> /root/.ssh/known_hosts
-chmod 0400 /root/.ssh/known_hosts
+ssh-keyscan gitlab.internal.sanger.ac.uk >> /home/ubuntu/.ssh/known_hosts
+chmod 0400 /home/ubuntu/.ssh/known_hosts
 
-apt update -y
-apt install git -y
-pip install ansible
-/usr/local/bin/ansible-pull ansible/main.yml -C dev -U $REPO -fi localhost, --full --purge
+sudo apt update -y
+sudo apt-get install git -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install ansible -y
+/usr/bin/ansible-pull ansible/main.yml -C dev -U $REPO -fi localhost --skip-tags=hosts
