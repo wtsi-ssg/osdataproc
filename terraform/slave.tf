f59ea8862ce65abb98859e50d6e9922d99cb0a26
Andrew McIsaac <am43@sanger.ac.uk>
2020-01-29 13:04:57 +0000

    Slave and Master use same flavor, and Spark executor/driver configured to use available_memory - 2048mb

--- terraform/slave.tf
+++ terraform/slave.tf
@@ -15,7 +15,7 @@ resource "openstack_compute_instance_v2" "spark_slave" {
   name            = format("${var.os_user_name}_${var.cluster_name}_slave_%02d",count.index+1)
   count           = var.slaves
   image_name      = var.image_name
-  flavor_name     = var.slave_flavor_name
+  flavor_name     = var.flavor_name
   key_pair        = var.spark_keypair == "None" ? openstack_compute_keypair_v2.spark_keypair[0].id : var.spark_keypair
   security_groups = [openstack_networking_secgroup_v2.spark_slave.id]
 
