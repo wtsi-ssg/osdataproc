3b31da3f896a7b517be9ed7850536375cf40cfb1
Andrew McIsaac <am43@sanger.ac.uk>
2020-01-31 09:27:00 +0000

    Remove count from master

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "openstack_networking_floatingip_v2" "public_ip" {
 resource "openstack_compute_floatingip_associate_v2" "public_ip" {
   count       = 1
   floating_ip = var.spark_master_public_ip == "" ? openstack_networking_floatingip_v2.public_ip[count.index].address : var.spark_master_public_ip
-  instance_id = openstack_compute_instance_v2.spark_master[count.index].id
+  instance_id = openstack_compute_instance_v2.spark_master.id
 }
 
 resource "openstack_compute_keypair_v2" "spark_keypair" {
