2e0a35adf007a93602d25f6c5e1a50e57d175edd
Andrew McIsaac <am43@sanger.ac.uk>
2020-03-19 11:05:08 +0000

    nfs mount from Terraform

--- terraform/master.tf
+++ terraform/master.tf
@@ -66,3 +66,9 @@ resource "openstack_compute_instance_v2" "spark_master" {
     command = "echo '[spark_master]\nubuntu@${openstack_networking_floatingip_v2.public_ip.0.address}' > terraform.tfstate.d/${var.cluster_name}/hosts_master"
   }
 }
+
+resource "openstack_compute_volume_attach_v2" "spark_volume" {
+  count       = var.nfs_volume_id == "" ? 0 : 1
+  instance_id = openstack_compute_instance_v2.spark_master.id
+  volume_id   = var.nfs_volume_id
+}
